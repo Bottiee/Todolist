27583ee4bcf52fe239e76150bd5c57847e321a82
Bottiee <siaulys.manfredas@gmail.com>
2025-07-07 08:26:23 +0300

    Some minor polish

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,3 +5,8 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'completed']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control w-75 rounded-3', 'rows': 3}),
+            'completed': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
